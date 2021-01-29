e9f00d7761b3242f47974cd0c0ae1a777d305627
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-01-29 17:51:37 -0600

    first commit successfully mapped drumpadKeys array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+const drumpadKeys = [
+  {key: 'Q', source: "src"},
+  {key: 'W', source: "src"},
+  {key: 'E', source: "src"},
+  {key: 'A', source: "src"},
+  {key: 'S', source: "src"},
+  {key: 'D', source: "src"},
+  {key: 'Z', source: "src"},
+  {key: 'X', source: "src"},
+  {key: 'C', source: "src"},
+]
+
+const DrumPad = () => {
+  const drumpadKeyContainers = drumpadKeys.map((item) =>
+    <div className="drum-pad" id={item.key}>
+      <audio className="clip" src={item.source} />
+      {item.key}, {item.source}
+    </div>
+  )
+  return (
+    <div>{drumpadKeyContainers}</div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine">
+      <div id="display">
+        Hello World
+        <DrumPad />
+      </div>
     </div>
-  );
+  ) 
 }
 
 export default App;
