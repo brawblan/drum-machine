cbf83c65904346cfe7671e3429b5416a90f7debe
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-01-29 22:44:10 -0600

    edited UI, added keycode key in drumpad object

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import './App.css';
 
 
 const drumpadKeys = [
-  {key: 'Q', source: "src"},
-  {key: 'W', source: "src"},
-  {key: 'E', source: "src"},
-  {key: 'A', source: "src"},
-  {key: 'S', source: "src"},
-  {key: 'D', source: "src"},
-  {key: 'Z', source: "src"},
-  {key: 'X', source: "src"},
-  {key: 'C', source: "src"},
+  {key: 'Q', source: "src", keycode: 80},
+  {key: 'W', source: "src", keycode: 80},
+  {key: 'E', source: "src", keycode: 80},
+  {key: 'A', source: "src", keycode: 80},
+  {key: 'S', source: "src", keycode: 80},
+  {key: 'D', source: "src", keycode: 80},
+  {key: 'Z', source: "src", keycode: 80},
+  {key: 'X', source: "src", keycode: 80},
+  {key: 'C', source: "src", keycode: 80},
 ]
 
 const DrumPad = () => {
   const drumpadKeyContainers = drumpadKeys.map((item) =>
-    <div className="drum-pad" id={item.key}>
-      <audio className="clip" src={item.source} />
-      {item.key}, {item.source}
+    <div className="drum-pad" key={item.key}>
+      <audio className="clip" id={item.key} src={item.source} />
+      {item.key}
     </div>
   )
   return (
-    <div>{drumpadKeyContainers}</div>
+    <div id="drumpadKeyContainer">{drumpadKeyContainers}</div>
   )
 }
 
@@ -31,7 +31,6 @@ function App() {
   return (
     <div id="drum-machine">
       <div id="display">
-        Hello World
         <DrumPad />
       </div>
     </div>
