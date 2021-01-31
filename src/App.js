973bfe736a3b13e313bc9b7bd1ce212076662e50
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-01-30 22:21:10 -0600

    added keycodes, working on keyPress event

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import './App.css';
 
 
 const drumpadKeys = [
-  {key: 'Q', source: "src", keycode: 80},
-  {key: 'W', source: "src", keycode: 80},
-  {key: 'E', source: "src", keycode: 80},
-  {key: 'A', source: "src", keycode: 80},
-  {key: 'S', source: "src", keycode: 80},
-  {key: 'D', source: "src", keycode: 80},
-  {key: 'Z', source: "src", keycode: 80},
-  {key: 'X', source: "src", keycode: 80},
-  {key: 'C', source: "src", keycode: 80},
+  {key: 'Q', source: "src", keycode: 81},
+  {key: 'W', source: "src", keycode: 87},
+  {key: 'E', source: "src", keycode: 69},
+  {key: 'A', source: "src", keycode: 65},
+  {key: 'S', source: "src", keycode: 83},
+  {key: 'D', source: "src", keycode: 68},
+  {key: 'Z', source: "src", keycode: 90},
+  {key: 'X', source: "src", keycode: 88},
+  {key: 'C', source: "src", keycode: 67},
 ]
+
+const handlePress = (e) => {
+  console.log(e.keycode);
+}
 
 const DrumPad = () => {
   const drumpadKeyContainers = drumpadKeys.map((item) =>
-    <div className="drum-pad" key={item.key}>
-      <audio className="clip" id={item.key} src={item.source} />
+    <div 
+      className="drum-pad" 
+      key={item.key} 
+      keycode={item.keycode}
+      onKeyDown={(e) => handlePress(e)}
+    >
+      <audio 
+        className="clip" 
+        id={item.key} 
+        src={item.source} 
+      />
       {item.key}
     </div>
   )
@@ -25,7 +38,6 @@ const DrumPad = () => {
     <div id="drumpadKeyContainer">{drumpadKeyContainers}</div>
   )
 }
-
 
 function App() {
   return (
