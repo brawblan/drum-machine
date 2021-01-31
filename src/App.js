50f593e91ceb52398a63a403753086fd959ff740
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-01-31 17:17:58 -0600

    added useState for drumpad click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
@@ -14,14 +14,48 @@ const drumpadKeys = [
   {key: 'C', source: "src", keycode: 67},
 ]
 
+const notClicked = {
+  border: '3px inset rgb(120, 120, 120)',
+  width: '100px',
+  height: '100px',
+  backgroundColor: '#000',
+  color: '#fff',
+}
+
+const clicked = {
+  backgroundColor: '#ffffff',
+  color: '#000000',
+  border: '3px outset rgb(120, 120, 120)',
+  width: '100px',
+  height: '100px',
+  position: 'relative',
+  top: '2px',
+  right: '2px'
+};
+
 const DrumPad = () => {
 
+  const [isClicked, setIsClicked] = useState(true);
+
+  const keyPress = (e) => {
+    console.log(e.target)
+    console.log(e.keyCode)
+    drumpadKeys.forEach(item => {
+      if (e.keyCode === item.key) {
+        console.log(item.key)
+        setIsClicked(false)
+      }
+    })
+  }
+  
   const drumpadKeyContainers = drumpadKeys.map((item) =>
+
     <div 
-      className="drum-pad" 
+      className="drum-pad"
+      style={isClicked ? notClicked : clicked}
+      onKeyDown={keyPress}
       key={item.key} 
       keycode={item.keycode}
-      onKeyDown={() => (console.log(KeyboardEvent.code))} 
       tabIndex={0}
     >
       <audio 
@@ -41,7 +75,7 @@ function App() {
   return (
     <div id="drum-machine">
       <div id="display">
-        <DrumPad />
+        <DrumPad onKeyDown={() => console.log('hi')} />
       </div>
     </div>
   ) 
