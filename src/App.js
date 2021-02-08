4a565d6bd5370aaab149eee42e1dfbc7071b6c75
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-02-07 19:12:21 -0600

    using crypto tutorial to target individual pads

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import useSound from 'use-sound';
-
-const drumpadData = [
-  {id: 'cymbal', letter: 'Q', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', keycode: 81},
-  {id: 'snare', letter: 'W', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 87},
-  {id: 'hihat', letter: 'E', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 69},
-  {id: 'bass', letter: 'A', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 65},
-  {id: 'tom1', letter: 'S', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 83},
-  {id: 'tom2', letter: 'D', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 68},
-  {id: 'tom3', letter: 'Z', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 90},
-  {id: 'ride', letter: 'X', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 88},
-  {id: 'crash', letter: 'C', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 67},
-]
 
 // const notClicked = {
 //   border: '3px inset rgb(120, 120, 120)',
@@ -33,39 +21,22 @@ const drumpadData = [
 //   right: '2px'
 // };
 
-const DrumPad = ({ id, letter, src, handleDisplay }) => {
+const DrumPad = ({ pad }) => {
+  const { id, letter, src } = pad;
   // const [isClicked, setIsClicked] = useState(true);
   const [play] = useSound(src);
 
-  function useKey(key, callback) {
-    const callbackRef = useRef(callback);
-
-    useEffect(() => {
-      callbackRef.current = callback;
-    });
-
-    useEffect(() => {
-      function handle(e) {
-        if (e.code === key) {
-          callbackRef.current(e);
-        }
-      }
-      document.addEventListener('keydown', handle);
-      return () => document.removeEventListener('keydown', handle)
-    });
-  }
 
   const handleKey = (e) => {
-    console.log(e.code)
+    console.log(e)
   }
 
-  useKey('KeyQ', handleKey)
   return (
     <div 
       className='drum-pad'
       id={id}
       onClick={play}
-      onKeyDown={useKey}
+      onKeyDown={handleKey}
     >
       <h1>{letter}</h1>
       <audio 
@@ -78,11 +49,24 @@ const DrumPad = ({ id, letter, src, handleDisplay }) => {
 } 
 
 function App() {
+  const drumpadData = [
+    {id: 'cymbal', letter: 'Q', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', keycode: 81},
+    {id: 'snare', letter: 'W', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 87},
+    {id: 'hihat', letter: 'E', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 69},
+    {id: 'bass', letter: 'A', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 65},
+    {id: 'tom1', letter: 'S', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 83},
+    {id: 'tom2', letter: 'D', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 68},
+    {id: 'tom3', letter: 'Z', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 90},
+    {id: 'ride', letter: 'X', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 88},
+    {id: 'crash', letter: 'C', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', keycode: 67},
+  ]
+  
   const [display, setDisplay] = useState('');
 
   const handleDisplay = (id) => {
     setDisplay(id)
   }
+
   return (
     <div id="drum-machine">
       <h1>{display}</h1>
@@ -91,9 +75,8 @@ function App() {
           {drumpadData.map((d) => (
             <DrumPad
               key={d.id}
-              id={d.id}
-              letter={d.letter}
-              src={d.src}
+              pad={drumpadData}
+              selected={d.id === setDisplay.id}
               handleDisplay={handleDisplay}
             />
           ))}
